bf24e8235dddff686f11ae89ed671c50f7bb7537
Rodrigo Goulart Ribeiro <rodrigo.goulart.ribeiro@gmail.com>
2020-10-19 21:43:42 -0300

    [WEB] Configure ESLint and run lint fixes

--- web/src/components/Sidebar.tsx
+++ web/src/components/Sidebar.tsx
@@ -7,17 +7,17 @@ import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/components/sidebar.css';
 
 export default function Sidebar() {
-  const { goBack } = useHistory();
-  
-  return (
-    <aside className="app-sidebar">
-      <img src={mapMarkerImg} alt="Happy" />
+    const { goBack } = useHistory();
 
-      <footer>
-        <button type="button" onClick={goBack}>
-          <FiArrowLeft size={24} color="#FFF" />
-        </button>
-      </footer>
-    </aside>
-  );
+    return (
+        <aside className="app-sidebar">
+            <img src={mapMarkerImg} alt="Happy" />
+
+            <footer>
+                <button type="button" onClick={goBack}>
+                    <FiArrowLeft size={24} color="#FFF" />
+                </button>
+            </footer>
+        </aside>
+    );
 }
